5a9c2ca52cc3b6604f46766429f0d85c9660f8f6
H20Beetle <waterbeetle091@gmail.com>
2023-09-25 19:36:45 -0500

    TS EX for 9/25

---
+++ JavaScript Foundations/Exercises.js
@@ -0,0 +1,22 @@
+// Exercise 1
+// Objective: Understand the basic concepts of JavaScript
+// Instructions: Write a JavaScript program that displays "Hello, World!" on the console.
+console.log('Hello, World!');
+// Exercise 2
+// Objective: Work with data types and variables in JavaScript
+// Instructions: Declare a variable named name and assign it your name as a string. Display the value of the name variable on the console.
+var Name = ('Kyle');
+console.log(Name);
+// Exercise 3
+// Objective: Use operators for mathematical computations and comparisons
+// Instructions: Write a JavaScript program that calculates and displays the area of a rectangle. Use variables length and width to store the dimensions of the rectangle.
+var Length = 30;
+var Width = 12;
+console.log(Length * Width);
+// Exercise 4
+// Objective: Implement conditional statements for decision-making
+// Instructions: Write a JavaScript program that checks if a number is even or odd. Display a message on the console indicating whether the number is even or odd.
+// if / else statement for even or odd  
+// Exercise 5
+// Objective: Control program flow using loops
+// Instructions: Write a JavaScript program that displays the numbers from 1 to 10 on the console using a for loop.
---
+++ JavaScript Foundations/Exercises.ts
@@ -0,0 +1,50 @@
+// Exercise 1
+// Objective: Understand the basic concepts of JavaScript
+// Instructions: Write a JavaScript program that displays "Hello, World!" on the console.
+console.log('Hello, World!');
+
+// Exercise 2
+// Objective: Work with data types and variables in JavaScript
+// Instructions: Declare a variable named name and assign it your name as a string. Display the value of the name variable on the console.
+const Name:string = ('Kyle');
+
+console.log(Name);
+
+// Exercise 3
+// Objective: Use operators for mathematical computations and comparisons
+// Instructions: Write a JavaScript program that calculates and displays the area of a rectangle. Use variables length and width to store the dimensions of the rectangle.
+const Length:number = 30;
+const Width:number = 12;
+
+console.log(Length * Width);
+
+
+
+// Exercise 4
+// Objective: Implement conditional statements for decision-making
+// Instructions: Write a JavaScript program that checks if a number is even or odd. Display a message on the console indicating whether the number is even or odd.
+
+// if / else statement for even or odd  
+
+if(number % 2==0){
+   
+    //The number is even
+ }
+ else {
+    
+    //The number is odd
+ }
+
+
+
+// Exercise 5
+// Objective: Control program flow using loops
+// Instructions: Write a JavaScript program that displays the numbers from 1 to 10 on the console using a for loop.
+
+
+// Exercise 6
+// Objective: Understand the basic concepts of JavaScript
+// Instructions: Write a JavaScript program that prompts the user for their name and displays a personalized greeting message on the console.
+// Starter Code
+
+const name = prompt("Enter your name: ");
